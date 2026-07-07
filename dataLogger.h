c451c741ccfc0856696fd81604f9e5aa0365c735
antroyster <anthonymichaelroyster@outlook.com>
2026-07-07 19:42:54 +0000

    Testing Files

--- dataLogger.h
+++ dataLogger.h
@@ -11,21 +11,28 @@
 #include <utility>
 #include <string>
 
-class dataLogger{
-    public:
+class dataLogger {
+public:
     dataLogger() = default;
+    struct Channel {
+        std::vector<double>data;
+        std::string id;
+        std::string units;
+        int index;
+    };
     void readStore();
+    void storeWrite(const std::vector<Channel>& channels, const std::vector<std::string>& metadata, std::vector<int>& rowInclusion,size_t numofRows);
     void getMetadata(std::ifstream& insertedFile);
     void getChannels(std::ifstream& insertedFile);
     void getData(std::ifstream& insertedFile);
-    private:
-        std::pair<double,double> criteria;
-        std::vector<std::string> metadata;     
-        struct Channel {
-            std::vector<double>data;
-            std::string id;
-            std::string units;
-        };
-        std::vector<Channel> channels;
+    const std::vector<Channel>& getchannelsData() const;
+    const std::vector<std::string>& getmetadata() const; 
+private:
+    std::vector<std::string> metadata;
+    std::vector<Channel> channels;
+    std::string fileName;
+    
 };
-#endif 
+
+#endif // DATALOGGER_H
+
